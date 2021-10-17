ad8b425aa639a54b1c0d754b1df4e8089dcc502d
d-s-2803 <dhruvilshah077@gmail.com>
2021-10-17 20:16:45 +0530

    Added Contact-List

---
+++ contact/contacts/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Contact(models.Model):
+    full_name = models.CharField(max_length=250)
+    phone_number = models.CharField(max_length=20)
+    address = models.CharField(max_length=1000)
+
+    def __str__(self):
+          return self.full_name
